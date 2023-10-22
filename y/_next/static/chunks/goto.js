c9c76dbca44bbaf2e89eb6735618201db657ea10
Omar Abuissa <120236901+OAbuissa@users.noreply.github.com>
2023-10-23 00:51:23 +0300

    10/23/2021 - 12:51 AM

---
+++ y/_next/static/chunks/goto.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector(".NavbarBody_itemsContainer__Jvm3k");
+
+    navbar.addEventListener('click', function() {
+        window.location.href = 'index.html';
+    });
+});
