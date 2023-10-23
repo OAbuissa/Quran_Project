4030c9c0ff24fddc69fb3f8f101fd12b84b8f2fc
Omar Abuissa <120236901+OAbuissa@users.noreply.github.com>
2023-10-24 02:43:29 +0300

    20/24/2023 - 2:43 AM

--- y/_next/static/chunks/goto.js
+++ y/_next/static/chunks/goto.js
@@ -1,17 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const navbar = document.querySelector(".NavbarBody_itemsContainer__Jvm3k");
+  const navbar = document.querySelector(".NavbarBody_itemsContainer__Jvm3k");
 
-    navbar.addEventListener('click', function() {
-        window.location.href = 'index.html';
-    });
-;
+  navbar.addEventListener("click", function () {
+    window.location.href = "index.html";
+  });
+  var linkElement = document.createElement("link");
+  linkElement.rel = "icon";
+  linkElement.href = "images/icon.png";
+  linkElement.type = "image/x-icon";
+  document.head.appendChild(linkElement);
 
+  // Get a reference to the element with the specific class
+  var elementToDelete = document.querySelector("");
 
-var linkElement = document.createElement("link");
-linkElement.rel = "icon";
-linkElement.href = "images/icon.png";
-linkElement.type = "image/x-icon";
-document.head.appendChild(linkElement);
-
+  // Check if the element exists
+  if (elementToDelete) {
+    // Remove the element and its contents
+    elementToDelete.parentNode.removeChild(elementToDelete);
+  }
 
 });
+(function () {
+  // Get all audio elements on the page.
+  const audioElements = document.querySelectorAll("audio");
+
+  // Mute all audio elements.
+  audioElements.forEach((element) => (element.muted = true));
+})();
---
+++ y/_next/static/css/main.js
@@ -0,0 +1,3 @@
+import { DotSpinner } from "@uiball/loaders";
+
+<DotSpinner size={40} speed={0.9} color="black" />;
